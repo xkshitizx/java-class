1f33a456b8a0d35e127bee9aa52113f9f874cad5
xkshitizx <kshitizlama03@gmail.com>
2020-05-03 20:45:17 +0545

    patch final
    final patch

--- file-writer/FW.java
+++ file-writer/FW.java
@@ -2,7 +2,7 @@ import java.io.FileWriter;
 public class FW {
     public static void main(String[] args){
          try{
-           FileWriter fl=new FileWriter("/home/kshitiz/Desktop/java/file-writer/files-filewriter/abc.txt");
+           FileWriter fl=new FileWriter("files-filewriter/abc.txt");
            fl.write("Kshitiz");
            fl.close();
            System.out.println("Done writing");
