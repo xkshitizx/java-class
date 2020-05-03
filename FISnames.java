666ebd50d1f1134ec0a8b3a0b14b59a61cf8c1e7
xkshitizx <kshitizlama03@gmail.com>
2020-05-03 18:31:27 +0545

    listed name in arrays and displayed each name

--- FISnames.java
+++ FISnames.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 public class FIS {
+    //declaring array
     public static String[] names=new String[10];
      public static void main(String[] args){
         try{
@@ -8,21 +9,27 @@ public class FIS {
             FileInputStream fis=new FileInputStream("/home/kshitiz/Desktop/java/file/kshitiz.txt");
             
             while((i=fis.read()) != -1){
-                names[ind]="";
-
-                while(boolean z!=true){
                 
-                    char x=(char)i;
-                    if(x=='\n'){
-                        System.out.println(names[ind]);
-                        ind+=1;
-                    }
-                    else{
-                    names[ind]=names[ind]+x;
-                    }
-                    z=true;
+                //initializing value of elements of array to be empty because by default they are null
+                if (names[ind]==null){
+                    names[ind]="";
                 }
+
+                char x=(char)i;
+
+                /*if new line is detected, the word stored in array is printed and next index is incremented
+                else concatenate the character to indexed element.
+                */
+                if(x=='\n'){
+                    System.out.println(names[ind]);
+                    ind+=1;
+                }
+                else{
+                    names[ind]=names[ind]+x;
+                }
+                
             }
+            
             fis.close();
           }catch(Exception e){System.out.println(e);}
          }
