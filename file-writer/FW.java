7f421f97662a8a538d9b21fd0311179d27ee3ae7
xkshitizx <kshitizlama03@gmail.com>
2020-05-03 19:10:23 +0545

    organized fos in a folder and created filewriter folder

---
+++ file-writer/FW.java
@@ -0,0 +1,13 @@
+import java.io.FileWriter;
+public class FW {
+    public static void main(String[] args){
+         try{
+           FileWriter fl=new FileWriter("/home/kshitiz/Desktop/java/file-writer/files-filewriter/abc.txt");
+           fl.write("Kshitiz");
+           fl.close();
+           System.out.println("Done writing");
+          }catch(Exception e){
+            System.out.println(e);
+          }
+        }
+}
