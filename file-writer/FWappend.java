15c1bf0064bef6aef5504fe2003eb0b2a16bf0f3
xkshitizx <kshitizlama03@gmail.com>
2020-05-03 19:38:27 +0545

    added other names usign filewriter

---
+++ file-writer/FWappend.java
@@ -0,0 +1,13 @@
+import java.io.FileWriter;
+public class FWappend {
+    public static void main(String[] args){
+         try{
+           FileWriter fadd=new FileWriter("/home/kshitiz/Desktop/java/file-writer/files-filewriter/abc.txt");
+           fadd.write("Kshitiz\nSaroj\nSaurav\nSanzay\nSamir");
+           fadd.close();
+           System.out.println("Done adding");
+          }catch(Exception e){
+            System.out.println(e);
+          }
+        }
+}
