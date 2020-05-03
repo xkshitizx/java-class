a2923d7897cf622fba6ccc6c51442d4e8bd16a1e
xkshitizx <kshitizlama03@gmail.com>
2020-05-03 16:04:14 +0545

    created read file to read a word

---
+++ FIS.java
@@ -0,0 +1,15 @@
+import java.io.FileInputStream;
+public class FIS{
+    public static void main(String[] args){
+        try {
+            FileInputStream newrd = new FileInputStream("/home/kshitiz/Desktop/java/file/kshitiz.txt");
+            int i=0;
+            while((i=newrd.read())!=-1){
+                System.out.print((char)i);
+            }
+            newrd.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
